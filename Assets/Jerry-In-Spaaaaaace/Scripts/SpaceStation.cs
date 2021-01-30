3326e4c6956388c93a8d91c2fabf5a88c89a61d0
J-Hayton <54622497+J-Hayton@users.noreply.github.com>
2021-01-30 14:57:38 +0000

    Game Manager
    sotheresagamemanagernowandithelpsloadsinthescenesandtrackthescore

--- Assets/Jerry-In-Spaaaaaace/Scripts/SpaceStation.cs
+++ Assets/Jerry-In-Spaaaaaace/Scripts/SpaceStation.cs
@@ -4,22 +4,40 @@ using UnityEngine;
 
 public class SpaceStation : MonoBehaviour
 {
-    public GameObject Sprite;
+    public GameManager GameManager;
     public float RotationSpeed;
+
+    private Vector3 StartPosition;
+    bool won = false;
+
+    private void Start()
+    {
+        StartPosition = transform.position;
+        GameManager = FindObjectOfType<GameManager>();
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Sprite.transform.Rotate(new Vector3(0, 0, Time.deltaTime * RotationSpeed));
+        transform.Rotate(new Vector3(0, 0, Time.deltaTime * RotationSpeed));
+
+        if(transform.position != StartPosition)
+            transform.position = StartPosition;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        var objectiveHit = collision.gameObject.GetComponent<Objective>() ?? null;
-
-        if(objectiveHit != null)
+        if (!won)
         {
-            //win
+            Debug.Log($"collider hit {collision.gameObject.name}");
+
+            var objectiveHit = collision.gameObject.GetComponent<Objective>() ?? null;
+
+            if (objectiveHit != null)
+            {
+                won = true;
+                StartCoroutine(GameManager.WinLevel(gameObject.scene.name));
+            }
         }
     }
 }
