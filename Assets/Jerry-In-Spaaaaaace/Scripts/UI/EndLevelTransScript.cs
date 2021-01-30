4e79d0b9cac1f45908d40deba91a1caa690b89d2
hideyoshi <06jstephenson@gmail.com>
2021-01-30 22:09:54 +0000

    Transison UI done

--- Assets/Jerry-In-Spaaaaaace/Scripts/UI/EndLevelTransScript.cs
+++ Assets/Jerry-In-Spaaaaaace/Scripts/UI/EndLevelTransScript.cs
@@ -6,24 +6,35 @@ using UnityEngine;
 
 public class EndLevelTransScript : MonoBehaviour
 { 
-    private bool onScreen = false;
+    public Animator William;
+    private bool Shown = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        LetThereBeLight();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (onScreen){
-        
-        }
+
     }
     public void ComeOnEileen(){
-
+        if (Shown == false){
+        William.SetTrigger("Open");
+        Shown = true;
+        }
     }
     public void ComeOffEileen(){
-
+        if (Shown){
+        William.SetTrigger("Close");
+        Shown = false;
+        }
+    }
+    public void BackInBlack(){
+        William.SetTrigger("FadeToBlack");
+    }
+    public void LetThereBeLight(){
+        William.SetTrigger("FadeToScreen");
     }
 }
