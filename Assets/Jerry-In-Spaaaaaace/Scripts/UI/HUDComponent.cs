2df92c606e435f5b938923afd8f18f64cb6f2b5f
MaidenJD <i.am@jamesmaiden.co.uk>
2021-01-30 15:24:20 +0000

    Pretty Marker make jam hap hap

--- Assets/Jerry-In-Spaaaaaace/Scripts/UI/HUDComponent.cs
+++ Assets/Jerry-In-Spaaaaaace/Scripts/UI/HUDComponent.cs
@@ -15,6 +15,9 @@ public class HUDComponent : MonoBehaviour
     [HideInInspector]
     private Objective Objective;
 
+    [HideInInspector]
+    private PlayerInput Ship;
+
     void Awake()
     {
         FuelGauge = transform.Find("Fuel Gauge").GetComponent<Slider>();
@@ -23,6 +26,8 @@ public class HUDComponent : MonoBehaviour
         OffScreenObjective = transform.Find("Objective Marker").Find("Off Screen Objective").GetComponent<Image>();
 
         Objective = GameObject.FindObjectOfType<Objective>();
+
+        Ship = GameObject.FindObjectOfType<PlayerInput>();
     }
 
     void Update()
@@ -34,7 +39,7 @@ public class HUDComponent : MonoBehaviour
         {
             Rect   ScreenRect   = Camera.main.pixelRect;
             Bounds ScreenBounds = new Bounds(ScreenRect.center, ScreenRect.size);
-            ScreenBounds.Expand(new Vector3(0, 0, float.PositiveInfinity));
+            ScreenBounds.Expand(new Vector3(0, 0, float.MaxValue));
 
             Vector3 ObjectiveScreenPos = Camera.main.WorldToScreenPoint(Objective.transform.position);
 
@@ -49,7 +54,26 @@ public class HUDComponent : MonoBehaviour
             }
             else
             {
-                OffScreenObjective.rectTransform.position = Vector3.Min(Vector3.Max(ObjectiveScreenPos, Vector3.zero), ScreenRect.size);
+                Vector2 HalfSize = OffScreenObjective.GetPixelAdjustedRect().size / 2;
+                //ScreenBounds.Expand(-HalfSize);
+
+                Debug.Log($"ObjectiveSreenPos: {ObjectiveScreenPos}");
+                Debug.Log($"HalfSize:          {HalfSize}");
+                Debug.Log($"ScreenBounds.size: {ScreenBounds.size}");
+
+                Vector3 Pos = new Vector3(
+                    Mathf.Clamp(ObjectiveScreenPos.x, HalfSize.x, ScreenBounds.size.x - HalfSize.x),
+                    Mathf.Clamp(ObjectiveScreenPos.y, HalfSize.y, ScreenBounds.size.y - HalfSize.y),
+                    1
+                );
+
+                OffScreenObjective.rectTransform.position = Pos;
+
+                Vector3 ShipScreenPos = Camera.main.WorldToScreenPoint(Ship.transform.position);
+
+                float Angle = Mathf.Atan2(ShipScreenPos.y - ObjectiveScreenPos.y, ShipScreenPos.x - ObjectiveScreenPos.x) * Mathf.Rad2Deg + 90;
+                Quaternion Rot = Quaternion.Euler(Vector3.forward * Angle);
+                OffScreenObjective.rectTransform.rotation = Rot;
             }
         }
     }
