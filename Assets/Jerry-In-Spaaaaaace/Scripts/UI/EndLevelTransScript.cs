3b2eaba978bb5779f9ccd1d0b3df34c6180efb40
hideyoshi <06jstephenson@gmail.com>
2021-01-31 04:03:57 +0000

    All done with GameOver and Fade to black

--- Assets/Jerry-In-Spaaaaaace/Scripts/UI/EndLevelTransScript.cs
+++ Assets/Jerry-In-Spaaaaaace/Scripts/UI/EndLevelTransScript.cs
@@ -9,9 +9,12 @@ public class EndLevelTransScript : MonoBehaviour
     public Animator William;
     private bool Shown = false;
     // Start is called before the first frame update
+    void Awake() {
+    }
     void Start()
     {
-        LetThereBeLight();
+        
+        ShowOnTheRoad();
     }
 
     // Update is called once per frame
@@ -19,22 +22,46 @@ public class EndLevelTransScript : MonoBehaviour
     {
 
     }
+    //shows the level end screen
     public void ComeOnEileen(){
         if (Shown == false){
         William.SetTrigger("Open");
         Shown = true;
         }
     }
+    //hides the level end screen and fades to black
     public void ComeOffEileen(){
         if (Shown){
         William.SetTrigger("Close");
         Shown = false;
         }
     }
+    //Fades to Black
     public void BackInBlack(){
         William.SetTrigger("FadeToBlack");
     }
+    //Fades in from Black
     public void LetThereBeLight(){
         William.SetTrigger("FadeToScreen");
+    }
+    //Fades in from Black and shows the Start Screen
+    public void ShowOnTheRoad(){
+        William.SetTrigger("OpenStart");
+    }
+    //Hides the Starting screen
+    public void EngageLevel(){
+        William.SetTrigger("CloseStart");
+    }
+    //Show just the Start Screen
+    public void WhatWasTheObjectiveAgain(){
+        William.SetTrigger("ShowStart");
+    }
+    //Shows GameOver Screen
+    public void GameOverManGameOver(){
+        William.SetTrigger("OpenOver");
+    }
+    //Hides GameOver Screen
+    public void WellMeetAgain(){
+        William.SetTrigger("CloseOver");
     }
 }
