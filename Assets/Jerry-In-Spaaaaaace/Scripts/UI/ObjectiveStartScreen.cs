c1f539d774cd7aa0d23b42422e3b1f59d7cf4a45
VGMurray <viviangmurray@hotmail.co.uk>
2021-01-31 13:35:12 +0000

    Objective updates on Start Screen

---
+++ Assets/Jerry-In-Spaaaaaace/Scripts/UI/ObjectiveStartScreen.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectiveStartScreen : MonoBehaviour
+{
+
+    public GameObject objectiveImageObject;
+    public GameObject objectiveTextObject;
+
+    private Objective objectiveRef;
+    private ObjectiveType currentObjectiveType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectiveTextObject = GameObject.Find("ObjectiveText");
+        objectiveImageObject = GameObject.Find("ObjectiveImg");
+
+        objectiveRef = FindObjectOfType<Objective>();
+        objectiveImageObject.GetComponent<Image>().sprite = objectiveRef.GetComponent<SpriteRenderer>().sprite;
+        objectiveTextObject.GetComponent<Text>().text = objectiveRef.objectiveName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
